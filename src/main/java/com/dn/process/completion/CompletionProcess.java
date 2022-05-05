d63d14b28eeb4667ede35fab9f29394fc000db56
dingning <dingning2333@163.com>
2022-05-05 22:08:21 +0800

    init 本地代码同步

---
+++ src/main/java/com/dn/process/completion/CompletionProcess.java
@@ -0,0 +1,10 @@
+package com.dn.process.completion;
+
+import com.dn.process.Process;
+
+/**
+ * @author dingning
+ * @date 2022/5/5 21:08
+ **/
+public interface CompletionProcess extends Process {
+}
