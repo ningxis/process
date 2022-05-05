6ecae3deed2b5bc3a14dcd49dd4532369681604e
dingning <dingning2333@163.com>
2022-05-05 21:51:27 +0800

    Revert "1、新增处理节点 2、测试代码覆盖"
    This reverts commit ce24f1c6

--- src/main/java/com/dn/service/ActivityServiceImpl.java
+++ src/main/java/com/dn/service/ActivityServiceImpl.java
@@ -2,18 +2,13 @@ package com.dn.service;
 
 import com.dn.bean.ProcessApplicationContext;
 import com.dn.exception.ServiceResult;
-import com.dn.process.ActivityInitProcess;
 import com.dn.process.DefaultProcessExecutor;
 import com.dn.process.PostProcess;
-import com.dn.process.completion.CompletionProcess;
-import com.dn.process.validator.DefaultValidatorProcess;
-import com.dn.process.validator.ValidatorProcess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
 
 /**
  * @author dingning
@@ -31,14 +26,7 @@ public class ActivityServiceImpl {
     //在容器初始化前处理执行器
     @PostConstruct
     public void init(){
-        //前置校验
-        executor.addNode(context.getBean(DefaultValidatorProcess.class));
-        //
         executor.addNode(context.getBean(PostProcess.class));
-        //地址信息补全
-        executor.addNode(context.getBean(CompletionProcess.class));
-
-        executor.addNode(context.getBean(ActivityInitProcess.class));
     }
 
 
