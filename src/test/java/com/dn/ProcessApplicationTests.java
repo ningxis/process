e6750716dbc6b46274c002371ab28c34c021a9a9
dingning <dingning2333@16.com>
2022-05-13 14:35:00 +0800

    ComletableFuture

--- src/test/java/com/dn/ProcessApplicationTests.java
+++ src/test/java/com/dn/ProcessApplicationTests.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
 @SpringBootTest
 class ProcessApplicationTests {
 
@@ -21,6 +28,21 @@ class ProcessApplicationTests {
         context.setUserId(11);
         ServiceResult result = activityService.activityTrigger(context);
         System.out.println(result);
+    }
+
+    @Test
+    // 参考文章 https://blog.csdn.net/admin123404/article/details/111168902
+    void testAsync() throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = new ForkJoinPool();
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println("Hello World"),threadPool);
+        System.out.println(future.get());
+        //如果方法存在executor参数，就使用executor执行任务;
+        //否则默认使用公用的ForkJoinPool.commonPool()作为执行异步任务的线程池。
+        CompletableFuture<ProcessApplication> future1 = CompletableFuture.supplyAsync(ProcessApplication::new, threadPool);
+        System.out.println(future1.get());
+        //异步线程任务执行完后调用 whenComplete 方法
+        future1.whenComplete((processApplication, throwable) -> System.out.println("执行完成之后的回调"));
+
     }
 
 }
