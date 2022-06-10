5af1c7c74cc62c4a3e83da4e861b37432180e9b5
dingning <dingning2333@163.com>
2022-06-10 11:42:21 +0800

    hello world

--- src/test/java/com/dn/ProcessApplicationTests.java
+++ src/test/java/com/dn/ProcessApplicationTests.java
@@ -7,12 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
-import java.util.function.BiConsumer;
-import java.util.function.Supplier;
 
 @SpringBootTest
 class ProcessApplicationTests {
@@ -37,10 +36,10 @@ class ProcessApplicationTests {
     }
 
     @Test
-    // 参考文章 https://blog.csdn.net/admin123404/article/details/111168902
+        // 参考文章 https://blog.csdn.net/admin123404/article/details/111168902
     void testAsync() throws ExecutionException, InterruptedException {
         ExecutorService threadPool = new ForkJoinPool();
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println("Hello World"),threadPool);
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println("Hello World"), threadPool);
         System.out.println(future.get());
         //如果方法存在executor参数，就使用executor执行任务;
         //否则默认使用公用的ForkJoinPool.commonPool()作为执行异步任务的线程池。
@@ -49,6 +48,26 @@ class ProcessApplicationTests {
         //异步线程任务执行完后调用 whenComplete 方法
         future1.whenComplete((processApplication, throwable) -> System.out.println("执行完成之后的回调"));
 
+    }
+
+    @Test
+    void testHelloWorld(){
+        System.out.println(randomString(-229985452) + " " + randomString(-147909649));
+    }
+
+
+
+    public static String randomString(int i) {
+        Random ran = new Random(i);
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int k = ran.nextInt(27);
+            if (k == 0)
+                break;
+
+            sb.append((char) ('`' + k));
+        }
+        return sb.toString();
     }
 
 }
