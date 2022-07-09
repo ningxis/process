a1c1964cbab919805ae27ef62704f408d92acbbe
dingning <dingning2333@163.com>
2022-07-09 12:49:48 +0800

    cycle print abc

---
+++ src/main/java/com/dn/algorithm/leetcode/NineWeek.java
@@ -0,0 +1,55 @@
+package com.dn.algorithm.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dingning
+ * @date 2022/7/9 上午 11:48
+ **/
+public class NineWeek {
+
+    private static String flag = "A";
+
+    private static int printSize = 35;
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+
+    }
+
+    //循环打印abc
+    //只能打印28次
+    @Test
+    public void cyclePrintABC() {
+        Thread threadA = new Thread(() -> {
+            while (true) {
+                if ("A".equals(flag) && count < printSize) {
+                    count++;
+                    System.out.println("第" + count + "次打印");
+                    System.out.println("A");
+                    flag = "B";
+                }
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            while (true) {
+                if ("B".equals(flag)  && count < printSize) {
+                    System.out.println("B");
+                    flag = "C";
+                }
+            }
+        });
+        Thread threadC = new Thread(() -> {
+            while (true) {
+                if ("C".equals(flag)  && count < printSize) {
+                    System.out.println("C");
+                    flag = "A";
+                }
+            }
+        });
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
