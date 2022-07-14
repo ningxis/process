ed637fca9cca4ddd2fd803964916ea2fabbef009
dingning <dingning2333@163.com>
2022-07-14 13:58:28 +0800

    leetCode 66

--- src/main/java/com/dn/algorithm/leetcode/NineWeek.java
+++ src/main/java/com/dn/algorithm/leetcode/NineWeek.java
@@ -14,11 +14,10 @@ import java.util.concurrent.FutureTask;
  * dingning 2022/7/9 下午 11:55  未能实现可配置化，也不能完全循环打印，还有线程间通信写法未尝试
  * 晚上是安静的好时候，适合写代码思考，有看到一些导航网站，真的挺棒的，感叹互联网真的给了我们太多便利，信息差就是我们效率的提升方向
  * 想做的还很多，继续加油
- *
- *         世界上唯有两样东西能让我们的内心受到深深的震撼，一是我们头顶浩瀚灿烂的星空，一是我们心中崇高的道德法则。
- *         ——康德《实践理性批判》
- *
- *          */
+ * <p>
+ * 世界上唯有两样东西能让我们的内心受到深深的震撼，一是我们头顶浩瀚灿烂的星空，一是我们心中崇高的道德法则。
+ * ——康德《实践理性批判》
+ */
 public class NineWeek {
 
     private static String flag = "A";
@@ -26,7 +25,6 @@ public class NineWeek {
     private static int printSize = 35;
 
     private static int count = 0;
-
 
 
     //循环打印abc
@@ -45,7 +43,7 @@ public class NineWeek {
         });
         Thread threadB = new Thread(() -> {
             while (true) {
-                if ("B".equals(flag)  && count < printSize) {
+                if ("B".equals(flag) && count < printSize) {
                     System.out.println("B");
                     flag = "C";
                 }
@@ -53,7 +51,7 @@ public class NineWeek {
         });
         Thread threadC = new Thread(() -> {
             while (true) {
-                if ("C".equals(flag)  && count < printSize) {
+                if ("C".equals(flag) && count < printSize) {
                     System.out.println("C");
                     flag = "A";
                 }
@@ -65,8 +63,8 @@ public class NineWeek {
     }
 
     @Test
-    public void cyclePrintABCByCallable(){
-        FutureTask<String> futureTask = new FutureTask<>(()->{
+    public void cyclePrintABCByCallable() {
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
             System.out.println("A");
             return "A";
         });
@@ -79,12 +77,12 @@ public class NineWeek {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int tmp = nums1[0];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(tmp >= nums2[j] || nums1[i] == 0){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tmp >= nums2[j] || nums1[i] == 0) {
                     tmp = nums1[i];
                     nums1[i] = nums2[j];
-                }else{
+                } else {
                     tmp = nums2[j];
                     break;
                 }
@@ -93,7 +91,7 @@ public class NineWeek {
         System.out.println(Arrays.toString(nums1));
     }
 
-    private static void mergeCopy(int[] nums1, int m, int[] nums2, int n){
+    private static void mergeCopy(int[] nums1, int m, int[] nums2, int n) {
         if (nums1 == null && nums2 == null) {
             return;
         }
@@ -105,7 +103,7 @@ public class NineWeek {
             if (nums1[left] > nums2[right]) {
                 tmp[index] = nums2[right];
                 right++;
-            }else{
+            } else {
                 tmp[index] = nums1[left];
                 left++;
             }
@@ -126,7 +124,7 @@ public class NineWeek {
         }
     }
 
-    private static void mergeCopy1(int[] nums1, int m, int[] nums2, int n){
+    private static void mergeCopy1(int[] nums1, int m, int[] nums2, int n) {
         //1.check
         if (nums1 == null && nums2 == null) {
             return;
@@ -159,14 +157,14 @@ public class NineWeek {
         }
     }
 
-    private static int leetCode26(int[] nums){
-        if(nums.length == 0){
+    private static int leetCode26(int[] nums) {
+        if (nums.length == 0) {
             return 0;
         }
         //用快慢指针解决问题
         int tmp = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[tmp] != nums[i]){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[tmp] != nums[i]) {
                 tmp++;
                 nums[tmp] = nums[i];
             }
@@ -175,12 +173,12 @@ public class NineWeek {
         return tmp + 1;
     }
 
-    private static int leetCode26Copy(int[] nums){
-        if(nums == null || nums.length == 0) return 0;
+    private static int leetCode26Copy(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
         int p = 0;
         int q = 1;
-        while(q < nums.length){
-            if(nums[p] != nums[q]){
+        while (q < nums.length) {
+            if (nums[p] != nums[q]) {
                 nums[p + 1] = nums[q];
                 p++;
             }
@@ -190,15 +188,15 @@ public class NineWeek {
         return p + 1;
     }
 
-    private static int leetCode28(String haystack, String needle){
-        if(haystack.length() == 0 || needle.length() == 0){
+    private static int leetCode28(String haystack, String needle) {
+        if (haystack.length() == 0 || needle.length() == 0) {
             return 0;
         }
-        if(haystack.equals(needle)){
+        if (haystack.equals(needle)) {
             return 0;
         }
         for (int i = 0; i < haystack.length(); i++) {
-            if(haystack.startsWith(needle, i)){
+            if (haystack.startsWith(needle, i)) {
                 return i;
             }
         }
@@ -207,10 +205,10 @@ public class NineWeek {
 
     //kmp算法
     public int strStr(String text, String pattern) {
-        if(text == null || pattern == null){
+        if (text == null || pattern == null) {
             return -1;
         }
-        if(pattern.length() == 0){
+        if (pattern.length() == 0) {
             return 0;
         }
         // 首先得到前缀表
@@ -222,18 +220,18 @@ public class NineWeek {
         int j = 0;
         char[] charsT = text.toCharArray();
         char[] charsP = pattern.toCharArray();
-        while(j < charsP.length && i < charsT.length){
+        while (j < charsP.length && i < charsT.length) {
             // 当指针i和j位置上的元素不匹配时，需要将j指针通过next数组移动到指定位置上
-            if(charsT[i] != charsP[j]){
+            if (charsT[i] != charsP[j]) {
                 j = next[j];
                 // 如果j等于-1，说明j指针已经pattern的最前面了，并且已经没有共有字符串了，直接将j指针和i指针同时往后移动一位j++
                 // 这里就能体现将next[0]设置成-1的巧妙了，这样j++，i++的时候就意味着把text当做一个全新的字符串，除去了[0,i)之前的元素，j=0也从pattern
                 // 的起始位置开始匹配
-                if(j == -1){
+                if (j == -1) {
                     i++;
                     j++;
                 }
-            }else{
+            } else {
                 //如果匹配上，那就正常进行下一个字符的匹配
                 i++;
                 j++;
@@ -241,13 +239,13 @@ public class NineWeek {
         }
         // 循环结束后，如果j和pattern长度相同，说明全部匹配完了，也就是在text中找到了第一次出现pattern的位置，这时候i指针已经到了pattern字符位置的最后一个字符的位置
         // 起始位置需要减掉pattern的长度
-        if(j == charsP.length){
+        if (j == charsP.length) {
             return i - j;
         }
         return -1;
     }
 
-    public int[] getNext(String pattern){
+    public int[] getNext(String pattern) {
         // 先求出字符串的前缀表
         char[] charArr = pattern.toCharArray();
         int[] next = new int[charArr.length];
@@ -267,17 +265,17 @@ public class NineWeek {
             7   ABABCABA     3
             8   ABABCABAA    1
         */
-        while (i < charArr.length){
+        while (i < charArr.length) {
             // 1.举例：比如这次进来的字符串是上面的AB,此时上一次的共有字符串长度是len=0(因为上一次就一个A字符，没有共有字符串，当然是0)，
             // 要想判断这次共有字符串长度会不会加1，只需要判断这次的字符串AB比上次字符串A多出来的字符(也就是B)是不是和上次共有字符串长度位置上的字符相等
             // 也就是charArr[1(i)] == charArr[0(len)]?，这里是不等，所以不能加1
             // 2.比如这次进来的是ABA，上一次是AB，那么多出来的这个A和上次AB的共有字符串长度位置(len=0)上的字符是否相等，显然charArr[0] == charArr[2]，所以len++;
             // 3.再比如：这次进来的是ABAB,上一次是ABA,上一次的共有字符串长度是len=1，判断这次多出来的字符B是不是和charArr[1]相等，显然相等，len++;
-            if(charArr[i] == charArr[len]){
+            if (charArr[i] == charArr[len]) {
                 len++;
                 next[i] = len;
                 i++;
-            }else{
+            } else {
                 // 如果不相等，说明这次多出来的这个字符并没有让共有字符串的长度加1，而且，可能还会直接影响到上一次的共有字符串长度
                 // 这里的做法是：因为多出来一个字符，而且charArr[i] != charArr[len]，那这次已经不能拿上一次共有字符串位置上的字符来做比较了，必须拿上上一次的结果
                 // 比如：这次进来的是ABABC,上一次是ABAB，上一次共有字符串是AB,len=2,那charArr[2(len)]是A，和这次的多出来的C已经不一样了，那上次的len已经不能作为判断依据了，
@@ -286,9 +284,9 @@ public class NineWeek {
                 // 此时charArr[0(len)]是A，还是和C不相同，说明这次的len还是不能作为判断，理论上还得去那更早一次的len值，但是这时候有个临界情况，因为已经拿到第一次进来的len了，
                 // len拿不到更早一次的值了，或者说到这已经没有共有字符串了，说明这次加多出来的字符C。彻底让这个字符串ABABC没有了共有字符串，也就是len=0，可以放心的将这一轮字符串
                 // 的共有字符串长度设为0了，这轮len值设置完毕，i++，进行下一轮设置
-                if(len > 0){
-                    len = next[len-1];
-                }else{
+                if (len > 0) {
+                    len = next[len - 1];
+                } else {
                     next[i] = len;
                     i++;
                 }
@@ -297,8 +295,8 @@ public class NineWeek {
         // 到此，前缀表已经设置完毕，但是有个问题，就是next[0]和next[1]的位置一直都是0，为了后续使用的方便，需要将""和只有一个字符的字符串共有前缀区别开，
         // 而且，对共有字符串来说，前缀表的最后一项就是字符串本身的共有字符串长度，这个在实际使用的时候没有意义，所以直接将整个前缀表往后平移一位，空出来的
         // next[0]赋值为-1
-        for (int j = next.length  -1; j > 0; j--) {
-            next[j] = next[j-1];
+        for (int j = next.length - 1; j > 0; j--) {
+            next[j] = next[j - 1];
         }
         next[0] = -1;
 //        for (int m = 0; m < next.length; m++) {
@@ -307,14 +305,14 @@ public class NineWeek {
         return next;
     }
 
-    private static int leetCode35(int[] nums,int target){
+    private static int leetCode35(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i],i);
-            if(map.get(target) != null){
+            map.put(nums[i], i);
+            if (map.get(target) != null) {
                 return map.get(target);
             }
-            if(target < nums[i]){
+            if (target < nums[i]) {
                 return i;
             }
         }
@@ -323,7 +321,7 @@ public class NineWeek {
 
 
     //最大子数组和
-    private static int leetCode53(int[] nums){
+    private static int leetCode53(int[] nums) {
         int pre = 0, maxAns = nums[0];
         for (int x : nums) {
             pre = Math.max(pre + x, x); //当前位置的最大值
@@ -332,19 +330,61 @@ public class NineWeek {
         return pre;
     }
 
-    private static int leetCode58(String word){
+    private static int leetCode58(String word) {
         String[] s = word.split(" ");
         return s[s.length - 1].length();
+    }
+
+    //要考虑满10向前加1
+    private static int[] leetCode66(int[] digits) {
+        int[] ints = new int[digits.length + 1];
+        int last = digits[digits.length - 1] + 1;
+
+        digits[digits.length - 1] = last % 10;
+        if (last / 10 == 1) {
+            int flag = 1;
+            //从后往前遍历，满9就向前加一位，如果要增加数组大小
+            for (int i = digits.length - 2; i >= 0; i--) {
+                if ((digits[i] + flag) / 10 > 0) {
+                    ints[i + flag] = (digits[i] + flag) % 10;
+                    flag = (digits[i] + flag) / 10;
+                    digits[i] = (digits[i] + flag) % 10;
+                } else {
+                    digits[i] = digits[i] + flag;
+                    return digits;
+                }
+            }
+            if(flag == 1){
+                ints[0] = flag;
+                return ints;
+            }
+        }
+        return digits;
+    }
+
+    private static int[] leetCode66Copy(int[] digits){
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            digits[i] = (digits[i] + 1) % 10;
+            //只要不等于0就直接返回，等于0就继续往下累加（9+1往前进）
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[len + 1];
+        digits[0] = 1;
+        return digits;
     }
 
     //算法题目前碰到的几种解法：递归、滑动窗口、双指针、动态规划、分治
     public static void main(String[] args) {
 //        merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
-        Objects.equals("","");//比较两个对象的值是否相等，且不为空
+        Objects.equals("", "");//比较两个对象的值是否相等，且不为空
 //        System.out.println(leetCode26(new int[]{1,1,2}));
 //        System.out.println(leetCode26(new int[]{0,0,1,1,1,2,2,3,3,4}));
 //        System.out.println(leetCode28("hello","ll"));
-        System.out.println(leetCode53(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+//        System.out.println(leetCode53(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(Arrays.toString(leetCode66Copy(new int[]{8,9,9,9})));
     }
 
 
