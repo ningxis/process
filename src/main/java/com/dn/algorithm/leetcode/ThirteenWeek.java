bae12f841aae1cec51e9b0e09f65bcf0f6751276
dingning <dingning2333@163.com>
2022-08-08 21:27:01 +0800

    位运算

--- src/main/java/com/dn/algorithm/leetcode/ThirteenWeek.java
+++ src/main/java/com/dn/algorithm/leetcode/ThirteenWeek.java
@@ -56,10 +56,50 @@ public class ThirteenWeek {
     }
 
 
+    private static int leetCode191(int n){
+        n = 00000000000000000000000000001011;
+        int sum = 0;
+        while(n != 0){
+            n = n & (n - 1);
+            sum++;
+        }
+        return sum;
+    }
+
+
+    //用位运算实现加法，
+    /**
+     * @description:
+     * @author skyline
+     * @date 2022/8/8 21:26
+     * 我们的计算机其实就是通过上述的位运算实现加法运算的（通过加法器，加法器就是使用上述的方法实现加法的），
+     * 而程序语言中的+ - * /运算符只不过是呈现给程序员的操作工具，计算机底层实际操作的永远是形如0101的位，
+     * 所以说位运算真的很重要！
+     */
+    private static void sum(int num1, int num2){
+        // 迭代写法
+            int sum = num1 ^ num2;
+            int carry = (num1 & num2) << 1;
+            while(carry != 0){
+                int a = sum;
+                int b = carry;
+                sum = a ^ b;
+                carry = (a & b) << 1;
+            }
+    }
+    private static int add(int num1, int num2){
+        // 递归写法
+            if(num2 == 0)
+                return num1;
+            int sum = num1 ^ num2;
+            int carry = (num1 & num2) << 1;
+            return add(sum, carry);
+    }
+
     public static void main(String[] args) {
 //        for (int i = 0; i < 100; i++) {
 //            System.out.println( i+ "" + leetCode367(i));
 //        }
-        System.out.println(leetCode345("hello"));
+        System.out.println(leetCode191(1));
     }
 }
