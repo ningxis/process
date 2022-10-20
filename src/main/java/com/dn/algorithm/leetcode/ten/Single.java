cce09a110e526e15387bc12670c275cac63313e3
dingning <dingning2333@163.com>
2022-10-20 23:41:32 +0800

    check

---
+++ src/main/java/com/dn/algorithm/leetcode/ten/Single.java
@@ -0,0 +1,46 @@
+package com.dn.algorithm.leetcode.ten;
+
+/**
+ * @author dingning
+ * @date 2022/10/20 下午 11:07
+ **/
+public class Single {
+
+    private Single(){
+
+    }
+
+    //饿汉式
+    private static volatile Single single = new Single();
+
+
+    public static Single getInstance(){
+        return single;
+    }
+
+    //懒汉式
+    private static Single single1 = null;
+    public static Single getInstance1(){
+        if(single1 == null){
+            single1 = new Single();
+        }
+        return  single1;
+    }
+
+
+    private static volatile Single single2;
+
+    //双检锁
+    public static Single getInstance2(){
+        if(single2 == null){
+            synchronized (Single.class){
+                if(single2 == null){
+                    single2 = new Single();
+                }
+            }
+        }
+        return single2;
+    }
+
+
+}
--- src/main/java/com/dn/algorithm/leetcode/ten/ThirdWeek.java
+++ src/main/java/com/dn/algorithm/leetcode/ten/ThirdWeek.java
@@ -41,6 +41,20 @@ public class ThirdWeek {
         }
         return prev;
     }
+    private static ListNode reverse(ListNode head){
+        if(head == null){
+            return null;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next  = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
 
     //递归反转链表
     private static ListNode reverseByRecursive(ListNode head){
@@ -80,6 +94,7 @@ public class ThirdWeek {
         }
         return true;
     }
+
     //找出环形链表的环
     private static ListNode getCircle(ListNode node){
         if(node == null || node.next == null){
@@ -106,6 +121,7 @@ public class ThirdWeek {
         return null;
     }
 
+
     //坚持才是胜利 2022年10月19日23:50:43
     public static void main(String[] args) {
         int[] ints = new int[]{2, 7, 11, 15};
@@ -114,10 +130,149 @@ public class ThirdWeek {
         int[] nums = new int[]{1, 2, 3, 4, 5};
         ListNode listNode = intArrayToListNode(nums);
         System.out.println(JSONObject.toJSONString(listNode));
+
         ListNode listNode1 = reverseByRecursive(listNode);
         System.out.println(JSONObject.toJSONString(listNode1));
+        ListNode listNode2 = reverse(listNode1);
+        System.out.println(JSONObject.toJSONString(listNode2));
+        ListNode listNode3 = reverse2(listNode2);
+        System.out.println(JSONObject.toJSONString(listNode3));
+    }
+
+    private static ListNode reverse2(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode last = reverse2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    private static boolean circle(ListNode node){
+        if(node == null || node.next == null){
+            return false;
+        }
+        ListNode slow = node.next;
+        ListNode fast = node.next.next;
+        while(fast != slow){
+            if(fast == null ||fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
     }
 
 
+    private static ListNode findCircle(ListNode node){
+        if(node == null || node.next == null){
+            return null;
+        }
+        ListNode slow = node;
+        ListNode fast = node;
+        while(fast != null){
+            slow = slow.next;
+            if(fast.next != null){
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if(fast == slow){
+                ListNode start  = node;
+                while(start != slow){
+                    slow = slow.next;
+                    start = start.next;
+                }
+                return start;
+            }
+        }
+        return null;
+    }
+
+
+    private static int[] twoNumbers(int[] nums, int target){
+        if(nums.length == 0){
+            return new int[0];
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int tar = target - nums[i];
+            if(map.containsKey(tar)){
+                return new int[]{i, map.get(tar)};
+            } else{
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+
+    //迭代
+    private ListNode reverse1(ListNode node){
+        if(node == null){
+            return null;
+        }
+        ListNode pre = null;
+        ListNode cur = node;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    //递归
+    private static ListNode reverse3(ListNode node){
+        if(node == null || node.next == null){
+            return node;
+        }
+        ListNode last = reverse3(node.next);
+        node.next.next = node;
+        node.next= null;
+        return last;
+    }
+
+    private static boolean circle1(ListNode node){
+        if(node == null || node.next == null){
+            return false;
+        }
+        ListNode slow = node.next;
+        ListNode fast = node.next.next;
+        while(slow != fast){
+            if(fast == null || fast.next == null){
+                return  false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    private static ListNode findCircle1(ListNode node){
+        if(node == null || node.next == null){
+            return null;
+        }
+        ListNode slow = node;
+        ListNode fast = node;
+        while(fast != slow){
+            slow = slow.next;
+            if(fast == null || fast.next == null){
+                return null;
+            }
+            fast = fast.next.next;
+            if(fast == slow){
+                ListNode start = node;
+                while(start != slow){
+                    slow = slow.next;
+                    start = start.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 
 }
