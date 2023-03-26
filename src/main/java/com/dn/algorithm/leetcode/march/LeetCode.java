cb8b170a8c3de624ef2a2ba87c525472dc2ae91d
dingning <dingning2333@163.com>
2023-03-26 16:44:11 +0800

    最长递增子序列

--- src/main/java/com/dn/algorithm/leetcode/march/LeetCode.java
+++ src/main/java/com/dn/algorithm/leetcode/march/LeetCode.java
@@ -884,6 +884,27 @@ public class LeetCode extends LinkedHashMap<Integer, Integer> {
         return nums[pre1];
     }
 
+    //最长递增子序列
+    private static int lengthOfLIS(int[] nums) {
+        if(nums.length < 2){
+            return nums.length;
+        }
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < dp.length; i++) {
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
 
         //hello world
     public static void main(String[] args) {
