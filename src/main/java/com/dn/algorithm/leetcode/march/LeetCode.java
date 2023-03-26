016870f2fb1bd4477a0e08153c36b59ce147b850
dingning <dingning2333@163.com>
2023-03-26 18:40:33 +0800

    最佳买卖股票时机含冷冻期

--- src/main/java/com/dn/algorithm/leetcode/march/LeetCode.java
+++ src/main/java/com/dn/algorithm/leetcode/march/LeetCode.java
@@ -894,7 +894,7 @@ public class LeetCode extends LinkedHashMap<Integer, Integer> {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);//等于前面最大子序列+1
                 }
             }
         }
@@ -903,6 +903,42 @@ public class LeetCode extends LinkedHashMap<Integer, Integer> {
             max = Math.max(dp[i], max);
         }
         return max;
+    }
+
+    //最佳买卖股票时机含冷冻期
+    private static int maxProfit(int[] prices) {
+        if(prices.length < 2){
+            return 0;
+        }
+        /*
+所以对于每一天i，都有可能是三种状态： 0.不持股且当天没卖出,定义其最大收益dp[i][0]; 1.持股,定义其最大收益dp[i][1]； 2.不持股且当天卖出了，定义其最大收益dp[i][2]；
+
+ 一、第i天不持股且没卖出的状态dp[i][0]，也就是我没有股票，而且还不是因为我卖了它才没有的，那换句话说是从i-1天到第i天转移时，它压根就没给我股票！
+ 所以i-1天一定也是不持有，那就是不持有的两种可能：i-1天不持股且当天没有卖出dp[i-1][0]；
+ i-1天不持股但是当天卖出去了dp[i-1][2]； 所以： dp[i][0]=max(dp[i-1][0],dp[i-1][2])
+
+二、第i天持股dp[i][1]，今天我持股，来自两种可能： 1、要么是昨天我就持股，今天继承昨天的，也就是dp[i-1][1]，这种可能很好理解；
+2、要么：是昨天我不持股，今天我买入的，但前提是昨天我一定没卖！因为如果昨天我卖了，那么今天我不能交易！
+也就是题目中所谓“冷冻期”的含义，只有昨天是“不持股且当天没卖出”这个状态，我今天才能买入！所以是dp[i-1][0]-p[i]
+所以： dp[i][1]=max(dp[i-1][1],dp[i-1][0]-p[i])
+
+三、i天不持股且当天卖出了，这种就简单了，那就是说昨天我一定是持股的，要不然我今天拿什么卖啊，
+而持股只有一种状态，昨天持股的收益加上今天卖出得到的新收益，就是dp[i-1][1]+p[i]啦 所以：dp[i][2]=dp[i-1][1]+p[i]
+
+总结：最后一天的最大收益有两种可能，而且一定是“不持有”状态下的两种可能，把这两种“不持有”比较一下大小，返回即可
+
+         */
+        int len = prices.length;
+        int[][] dp = new int[len][3];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]);//不持股且当天没卖出
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);//持股
+            dp[i][2] = dp[ i - 1][1] + prices[i];//不持股当天卖出了
+        }
+        return Math.max(dp[len - 1][0], dp[len - 1][2]);
     }
 
 
