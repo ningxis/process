64ed9c5e0bebcfd58fb7e8dd42f53e9ffb538732
dingning <dingning2333@163.com>
2023-04-14 09:16:38 +0800

    设计链表

--- src/main/java/com/dn/algorithm/leetcode/april/LeetCode.java
+++ src/main/java/com/dn/algorithm/leetcode/april/LeetCode.java
@@ -10,7 +10,7 @@ public class LeetCode {
 
     public class ListNode {
         int val;
-        ListNode next;
+        ListNode next, prev;
 
         ListNode() {
         }
@@ -190,7 +190,7 @@ public class LeetCode {
         int min = nums.length;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            while(sum >= target){
+            while (sum >= target) {
                 end = i;
                 min = Math.min(min, end - start + 1);
                 sum -= nums[start];
@@ -210,22 +210,22 @@ public class LeetCode {
         int count = 1;
         int offset = 1;
         int[][] nums = new int[n][n];
-        int i,j;
-        while(loop-- > 0){
+        int i, j;
+        while (loop-- > 0) {
             //上边
-            for(j = startY; j < n - offset; j++){
+            for (j = startY; j < n - offset; j++) {
                 nums[startX][j] = count++;
             }
             //右边
-            for(i = startX; i < n - offset; i++){
+            for (i = startX; i < n - offset; i++) {
                 nums[i][j] = count++;
             }
             //下边
-            for(; j > startX; j--){
+            for (; j > startX; j--) {
                 nums[i][j] = count++;
             }
             //左边
-            for(; i > startY; i--){
+            for (; i > startY; i--) {
                 nums[i][j] = count++;
             }
             // 第二圈开始的时候，起始位置要各自加1， 例如：第一圈起始位置是(0, 0)，第二圈起始位置是(1, 1)
@@ -235,7 +235,7 @@ public class LeetCode {
             startY++;
             offset++;
         }
-        if(n % 2 == 1){
+        if (n % 2 == 1) {
             nums[mid][mid] = count;
         }
         return nums;
@@ -245,8 +245,8 @@ public class LeetCode {
         ListNode node = new ListNode(-1, head);
         ListNode cur = node;
         ListNode pre = node;
-        while(cur != null){
-            if(head.val != val){
+        while (cur != null) {
+            if (head.val != val) {
                 cur.next = head;
                 head = head.next;
                 cur = cur.next;
@@ -261,8 +261,173 @@ public class LeetCode {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2,3,1,2,4,3};
+        int[] nums = new int[]{2, 3, 1, 2, 4, 3};
         System.out.println(generateMatrix(4));
     }
 
+    /**
+     * Your MyLinkedList object will be instantiated and called as such:
+     * MyLinkedList obj = new MyLinkedList();
+     * int param_1 = obj.get(index);
+     * obj.addAtHead(val);
+     * obj.addAtTail(val);
+     * obj.addAtIndex(index,val);
+     * obj.deleteAtIndex(index);
+     */
+
+    //单链表
+
+    //记录链表中元素的数量  //size存储链表元素的个数
+    int size;
+    //记录链表的虚拟头结点和尾结点
+    ListNode head, tail;
+
+
+    //初始化链表
+    public LeetCode() {
+        size = 0;
+        head = new ListNode(0);
+
+        //双链表
+        //初始化操作
+        this.size = 0;
+        this.head = new ListNode(0);
+        this.tail = new ListNode(0);
+        //这一步非常关键，否则在加入头结点的操作中会出现null.next的错误！！！
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    //获取第index个节点的数值，注意index是从0开始的，第0个节点就是头结点
+    public int get(int index) {
+        //如果index非法，返回-1
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode currentNode = head;
+        //包含一个虚拟头节点，所以查找第 index+1 个节点
+        for (int i = 0; i <= index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.val;
+    }
+
+    //在链表最前面插入一个节点，等价于在第0个元素前添加
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    //在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    // 在第 index 个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+    // 如果 index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+    // 如果 index 大于链表的长度，则返回空
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        size++;
+        //找到要插入节点的前驱
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pred.next;
+        pred.next = toAdd;
+    }
+
+    //删除第index个节点
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        size--;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
+    }
+
+    //双链表
+    public int get2(int index) {
+        //判断index是否有效
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode cur = this.head;
+        //判断是哪一边遍历时间更短
+        if (index >= size / 2) {
+            //tail开始
+            cur = tail;
+            for (int i = 0; i < size - index; i++) {
+                cur = cur.prev;
+            }
+        } else {
+            for (int i = 0; i <= index; i++) {
+                cur = cur.next;
+            }
+        }
+        return cur.val;
+    }
+
+    public void addAtHead2(int val) {
+        //等价于在第0个元素前添加
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail2(int val) {
+        //等价于在最后一个元素(null)前添加
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex2(int index, int val) {
+        //index大于链表长度
+        if (index > size) {
+            return;
+        }
+        //index小于0
+        if (index < 0) {
+            index = 0;
+        }
+        size++;
+        //找到前驱
+        ListNode pre = this.head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        //新建结点
+        ListNode newNode = new ListNode(val);
+        newNode.next = pre.next;
+        pre.next.prev = newNode;
+        newNode.prev = pre;
+        pre.next = newNode;
+
+    }
+
+    public void deleteAtIndex2(int index) {
+        //判断索引是否有效
+        if (index < 0 || index >= size) {
+            return;
+        }
+        //删除操作
+        size--;
+        ListNode pre = this.head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next.next.prev = pre;
+        pre.next = pre.next.next;
+    }
 }
