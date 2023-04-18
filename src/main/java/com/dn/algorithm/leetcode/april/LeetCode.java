2002e0f9d3fc56fe9eb25a367e95ef2fb33e0004
dingning <dingning2333@163.com>
2023-04-18 23:16:59 +0800

    两数之和

--- src/main/java/com/dn/algorithm/leetcode/april/LeetCode.java
+++ src/main/java/com/dn/algorithm/leetcode/april/LeetCode.java
@@ -981,6 +981,38 @@ public class LeetCode {
         return str.substring(1, str.length() - 1).contains(s);
     }
 
+    //两数之和
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            //错误写法
+            // if (l1.next != null) {
+            //     l1 = l1.next;
+            // }
+            // if (l2.next != null) {
+            //     l2 = l2.next;
+            // }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
         System.out.println(repeatedSubstringPattern("abcabcabcabc"));
     }
