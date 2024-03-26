8eae8b959e34edae28b7a8cbd442b240323a64f9
dingning <dingning2333@163.com>
2024-03-26 22:59:57 +0800

    迪士尼英文测试题

--- src/main/java/com/dn/strategy/factory/Test.java
+++ src/main/java/com/dn/strategy/factory/Test.java
@@ -4,11 +4,20 @@ import com.dn.strategy.factory.fac.HuaweiFactory;
 import com.dn.strategy.factory.fac.XmFactory;
 import com.dn.strategy.factory.intf.KeyBoard;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author dingning
  * @date 2024/3/20 下午 03:51
  **/
 public class Test {
+
+    @Override
+    public String toString() {
+//        System.out.println("3413431241");
+        return "456789";
+    }
 
     //抽象工厂模式
     public static void main(String[] args) {
@@ -20,7 +29,13 @@ public class Test {
         xmKeyBoard.input();
         HuaweiFactory huaweiFactory = new HuaweiFactory();
         huaweiFactory.createMainFrame().run();
+        LocalDate of = LocalDate.of(2015, 4, 5);
+        System.out.println(of.format(DateTimeFormatter.ofPattern("MMMdd,yyyy")));
+        System.out.println(of.format(DateTimeFormatter.ofPattern("E,MMM dd,yyyy")));
+        System.out.println(of.format(DateTimeFormatter.ofPattern("MM/dd/yy")));
+
     }
+
 
 
 }
